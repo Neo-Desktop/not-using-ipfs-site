c9e6ecee39e40aa0c63465445326c2018602d667
Sydney Bizkut <neo-desktop@users.noreply.github.com>
2020-02-13 14:55:36 -0800

    better redirection script

--- static/ipfs.js
+++ static/ipfs.js
@@ -1,2 +1,11 @@
-if (window.ipfs !== undefined)
-	window.location.href = "ipns://" + window.location.host;
+if (window.ipfs !== undefined) {
+	var href = "ipns://" + window.location.host;
+	document.title = "IPFS Redirection"
+	document.querySelector("#intro > div > div > div > div > div > h1").innerHTML = "IPFS Found";
+	document.querySelector("body > nav > div > div.navbar-header.page-scroll > a").innerHTML = "IPFS Found";
+	document.querySelector("#intro > div > div > div > div > div > h3").innerHTML = "Please wait while we redirect you.";
+	document.querySelector("#main").remove();
+	document.querySelector("#social-share-section").remove();
+	document.querySelector("#intro > div > div > div > div > div").insertAdjacentHTML('beforeend', '<h4><a href="'+href+'">Click here if you are not immediately redirected</a></h4>');
+	window.location.href = href;
+}
